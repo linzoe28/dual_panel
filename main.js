fb6faa0831f5502b0c5904bb25d33b8f1f8c32ce
linzoe28 <zoe841228@gmail.com>
2019-05-14 11:24:52 +0800

    test

---
+++ main.js
@@ -0,0 +1,13 @@
+const {app,BrowserWindow}=require('electron');
+
+let win 
+function createWindow() {
+    win=new BrowserWindow({windth:800,height:600})
+    win.loadFile('index.html')
+    win.webContents.openDevTools()
+    win.on('closed',()=>{
+        win=null
+    })
+}
+
+app.on('ready',createWindow)
